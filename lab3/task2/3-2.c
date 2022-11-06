a45ecfdfb7376b2bda8971ef1e52abde444a3de9
Zhuraev Donish <90084216+Donish@users.noreply.github.com>
2022-11-06 14:47:41 +0300

    Сдать

--- lab3/task2/3-2.c
+++ lab3/task2/3-2.c
@@ -1,97 +1,191 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-void func1(int **arr, int *sz, int k, int l, int *flag)
+enum ERROR {
+    CORRECT = 0,
+    ONLY_POSITIVE = 1,
+    L_MORE = 2,
+    MEMORY = 3
+};
+
+double combination(double m, double k)
 {
-    if(l < 0 || k < 0){
-        *flag = 1;
-        return;
+    double res = 1.0;
+    if(fabs(k) < 0.000000001 || fabs(k - m) < 0.000000001){
+        return 1.0;
+    } else if(fabs(k - 1.0) < 0.000000001 || fabs((k + 1) - m) < 0.000000001){
+        return m;
     }
-    if(l > k){
-        *flag = 2;
-        return;
+    for(; k > 0.000000001; m--, k--){
+        res *= m / k;
     }
-
-    int count_1 = 0;
-    int num = 1 << (k - 1);
-    int num_cp;
-    int *tmp;
-    for(int i = num; i < num * 2; i++){
-        num_cp = i;
-        while(num_cp != 0){
-            if((num_cp & 1) == 1)
-                count_1++;
-            
-            num_cp >>= 1;
-        }
-        if(count_1 == l){
-            tmp = (int*)realloc(*arr, sizeof(int) * (*sz + 1));
-            if(tmp == NULL){
-                *flag = 3;
-                if(*sz != 0)
-                    free(arr);
-                return;
-            }
-            *arr = tmp;
-            (*arr)[*sz] = i;
-            (*sz)++;
-        }
-        count_1 = 0;
-    }
+    return res;
 }
 
-void func2(int **arr, int *sz, int k, int l, int *flag)
+void func1(int **arr, int *size, int k, int l, int *flag)
 {
-    if(l < 0 || k < 0){
-        *flag = 1;
+    if(l <= 0 || k <= 0){
+        *flag = ONLY_POSITIVE;
         return;
     }
     if(l > k){
-        *flag = 2;
+        *flag = L_MORE;
         return;
     }
 
-    int num = 1 << (k - 1);
-    if(k == l){
-        *sz = 1;
-        *arr = (int*)malloc(sizeof(int));
-        (*arr)[0] = num * 2 - 1;
-        return;
-    } else if(l == 1){
-        *sz = 1;
-        *arr = (int*)malloc(sizeof(int));
-        (*arr)[0] = num;
-        return;
-    }
+    int num;
+    if(k == l){ // если кол-во бит и единиц совпадает
 
-    int count_1, num_cp;
-    int *tmp;
-    for(int i = num + 1; i < num * 2 - 1; i++){
-        num_cp = i;
-        while(num_cp != 0){
-            if((num_cp & 1) == 0)
-                count_1 = 0;
-            else
-                count_1++;
-            
-            if(count_1 == l && ((num_cp >> 1) & 1) == 0){
-                tmp = (int*)realloc(*arr, sizeof(int) * (*sz + 1));
-                if(tmp == NULL){
-                    *flag = 3;
-                    if(*sz != 0)
-                        free(arr);
-                    return;
-                }
-                *arr = tmp;
-                (*arr)[*sz] = i;
-                (*sz)++;
-                break;
-            }
-            num_cp >>= 1;
+        *size = 1;
+        *arr = (int*)malloc(sizeof(int));
+        if(*arr == NULL){
+            *flag = MEMORY;
+            return;
         }
-        count_1 = 0;
+        (*arr)[0] = (1 << l) - 1;
+
+    } else if(l == 1){ // если кол-во единиц равно одному
+
+        *size = k;
+        *arr = (int*)malloc(sizeof(int) * *size);
+        if(*arr == NULL){
+            *flag = MEMORY;
+            return;
+        }
+        num = 1;
+        for(int i = 0; i < *size; i++){
+            (*arr)[i] = num;
+            num <<= 1;
+        }
+
+    } else if(l == k - 1){ // если l на 1 меньше, чем k
+
+        *size = k;
+        *arr = (int*)malloc(sizeof(int) * *size);
+        num = (1 << k) - 2;
+        int step = 1;
+        for(int i = k - 1; i >= 0; i--){
+            (*arr)[i] = num;
+            num = num - (step);
+            step <<= 1;
+        }
+
+    } else{ // в ином случае
+
+        num = (1 << l) - 1;
+        int num_border = (1 << k) - 2;
+        *size = combination(k, l);
+        *arr = (int*)malloc(sizeof(int) * *size);
+        if(*arr == NULL){
+            *flag = MEMORY;
+            return;
+        }
+
+        int num_cp, count_1 = 0, j = 1;
+        (*arr)[0] = num;
+        for(int i = num + 1; i < num_border; i++){
+            num_cp = i;
+            while(num_cp != 0){
+                if((num_cp & 1) == 1)
+                    count_1++;
+
+                num_cp >>= 1;
+            }
+            if(count_1 == l){
+                (*arr)[j] = i;
+                j++;
+            }
+            count_1 = 0;
+        }
+
     }
 
+}
+
+void func2(int **arr, int *size, int k, int l, int *flag)
+{
+    if(l <= 0 || k <= 0){
+        *flag = ONLY_POSITIVE;
+        return;
+    }
+    if(l > k){
+        *flag = L_MORE;
+        return;
+    }
+
+
+    int num;
+    if(k == l){
+
+        *size = 1;
+        *arr = (int*)malloc(sizeof(int));
+        if(*arr == NULL){
+            *flag = MEMORY;
+            return;
+        }
+        (*arr)[0] = (1 << l) - 1;
+
+    } else if(l == 1){
+
+        *size = k;
+        *arr = (int*)malloc(sizeof(int) * *size);
+        if(*arr == NULL){
+            *flag = MEMORY;
+            return;
+        }
+        num = 1;
+        for(int i = 0; i < *size; i++){
+            (*arr)[i] = num;
+            num <<= 1;
+        }
+
+    } else if(l == k - 1){
+
+        *size = 2;
+        *arr = (int*)malloc(sizeof(int) * *size);
+        if(*arr == NULL){
+            *flag = MEMORY;
+            return;
+        }
+        num = (1 << l) - 1;
+        (*arr)[0] = num;
+        num <<= 1;
+        (*arr)[1] = num;
+
+    } else{
+
+        int num_cp, count_1 = 0;
+        num = (1 << l) - 1;
+        int num_border = (1 << k) - 2;
+        *arr = (int*)malloc(sizeof(int) * (num_border - num));
+        if(*arr == NULL){
+            *flag = MEMORY;
+            return;
+        }
+        (*arr)[0] = num;
+        num++;
+        (*size)++;
+        for(int i = num; i < num_border; i++){
+            num_cp = i;
+            while(num_cp != 0){
+                if((num_cp & 1) == 0)
+                    count_1 = 0;
+                else
+                    count_1++;
+
+                if(count_1 == l && ((num_cp >> 1) & 1) == 0){
+                    (*arr)[*size] = i;
+                    (*size)++;
+                    break;
+                }
+                num_cp >>= 1;
+            }
+            count_1 = 0;
+
+        }
+
+    }
 }
 
 void printarr(int *arr, int size)
@@ -102,12 +196,11 @@ void printarr(int *arr, int size)
     printf("\n");
 }
 
-// 0 - все норм; 1 - введены отриц числа; 2 - l больше, чем k; 3 - не выделилась память; 
 int main()
 {
     int *res1 = NULL, *res2 = NULL;
-    int sz1 = 0, sz2 = 0;
-    int k, l, flag = 0;
+    int size1 = 0, size2 = 0;
+    int k, l, flag = CORRECT;
     printf("Enter the number of bits:\n");
     if(!scanf("%d", &k)){
         printf("Enter only integer number!\n");
@@ -119,36 +212,36 @@ int main()
         return 0;
     }
 
-    func1(&res1, &sz1, k, l, &flag);
-    if(flag == 1){
+    func1(&res1, &size1, k, l, &flag);
+    if(flag == ONLY_POSITIVE){
         printf("Enter only positive numbers!\n");
         return 0;
-    } else if(flag == 2){
+    } else if(flag == L_MORE){
         printf("The number of units can't be greater than the number of bits!\n");
         return 0;
-    } else if(flag == 3){
+    } else if(flag == MEMORY){
         printf("Memory wasn't allocated!\n");
         return 0;
     } else{
-        printf("There are %d %d-bit numbers with %d units in binary notation:\n", sz1, k, l);
-        printarr(res1, sz1);
+        printf("There are %d %d-bit numbers with %d units in binary notation:\n", size1, k, l);
+        printarr(res1, size1);
         printf("\n");
         free(res1);
     }
 
-    func2(&res2, &sz2, k, l, &flag);
-    if(flag == 1){
+    func2(&res2, &size2, k, l, &flag);
+    if(flag == ONLY_POSITIVE){
         printf("Enter only positive numbers!\n");
         return 0;
-    } else if(flag == 2){
+    } else if(flag == L_MORE){
         printf("The number of units can't be greater than the number of bits!\n");
         return 0;
-    } else if(flag == 3){
+    } else if(flag == MEMORY){
         printf("Memory wasn't allocated!\n");
         return 0;
     } else{
-        printf("There are %d %d-bit numbers with %d consecutive units in binary notation:\n", sz2, k, l);
-        printarr(res2, sz2);
+        printf("There are %d %d-bit numbers with %d consecutive units in binary notation:\n", size2, k, l);
+        printarr(res2, size2);
         free(res2);
     }
 
